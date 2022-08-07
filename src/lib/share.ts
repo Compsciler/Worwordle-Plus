82d5a4b6e870cf1f658810b94e82a107922578a9
Roger Wang <rogerwang2520@gmail.com>
2022-08-06 21:02:35 -0700

    Update shareable game link

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
 const parser = new UAParser()
 const browser = parser.getBrowser()
 const device = parser.getDevice()
-const gameUrl = 'rebrand.ly/worwordle-plus'
+const gameUrl = 'worwordleplus.herokuapp.com'
 
 export const shareStatus = (
   solution: string,
